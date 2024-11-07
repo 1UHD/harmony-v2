419e58ba8f6253d053ef5fcb563c7e50b9c13630
iUHD <jensjensen187@icloud.com>
2024-11-07 20:37:51 +0100

    added search on yt and other stuff

--- src/cogs/QueueCog.py
+++ src/cogs/QueueCog.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import platform
+import requests
+from bs4 import BeautifulSoup
+import re
 
 import src.tools.embeds as embed
 from src.tools.logging import logger
@@ -55,18 +58,34 @@ class QueueCog(commands.Cog):
 
     async def _play_next_song(self, ctx: commands.Context) -> None:
         if not ctx.voice_client:
-            await embed.send_error("the fucking voice client doesnt fucking exist and i dont fucking kinow why ", context=ctx)
+            await embed.send_error("the fucking voice client doesnt fucking exist and i dont fucking know why ", context=ctx)
             return
 
         if queue.get_length() < 1:
-            embed.send_error("Queue is empty.", context=ctx)
+            await embed.send_error("Queue is empty.", context=ctx)
             return
         
-        song = queue.get_first_song()
+        if not ctx.voice_client.is_playing():
+            song = queue.get_first_song()
+            queue.remove(0)
 
-        timer.start()
-        ctx.voice_client.play(song.audio, after=lambda e: self.bot.loop.create_task(self._play_next_song(ctx=ctx)))
-        await embed.send_embed(title="Song playing", description=song.title, context=ctx)
+            timer.start()
+            ctx.voice_client.play(song.audio, after=lambda e: ctx.bot.loop.create_task(self._play_next_song(ctx=ctx)))
+            await embed.send_embed(title="Song playing", description=song.title, context=ctx)
+
+    def _search_youtube(self, query: str, message: discord.Message | None = None) -> str:
+        url = f"https://www.youtube.com/results?search_query={query.replace(' ', '+')}"
+        rep = requests.get(url=url)
+
+        if rep.status_code != 200:
+            if message:
+                message.edit(embed=discord.Embed(title="An unexpected error occured.", color=discord.Color.red()))
+            return
+
+        soup = BeautifulSoup(rep.text, features="html.parser")
+        video_id = re.findall(r'"videoId":"(.*?)"', str(soup.find_all("script")))
+
+        return video_id[0]
 
     @commands.hybrid_group(name="queue", description="Provides information about the queue.")
     async def queue(self, ctx: commands.Context) -> None:
@@ -81,11 +100,38 @@ class QueueCog(commands.Cog):
         await self._handle_vc(ctx=ctx)
         await self._play_next_song(ctx=ctx)
 
-    @queue.command(name="add", description="adds a song to le playlist")
-    async def queue_add(self, ctx: commands.Context, url: str) -> None:
+    @commands.hybrid_group(name="add", description="Adds a song to the queue.")
+    async def add(self, ctx: commands.Context) -> None:
+        if not ctx.invoked_subcommand:
+            await embed.send_error(title="Wrong usage, see `/help add` for more info.")
+
+    @add.command(name="url", description="Adds a song to the queue via a YouTube link.")
+    async def add_url(self, ctx: commands.Context, url: str) -> None:
+        message = await embed.send_embed("Adding song to queue.", context=ctx, color=discord.Color.yellow())
+
         song = Song(url=url)
         queue.add(song=song)
-        await embed.send_embed(f"{song.title} has been added to the queue.", context=ctx)
+
+        await message.edit(embed=discord.Embed(
+            title=f"{song.title} has been added to the queue.",
+            color=discord.Color.blurple()
+        ))
+
+    @add.command(name="search", description="Adds a song to the queue via a prompt.")
+    async def add_search(self, ctx: commands.Context, prompt: str) -> None:
+        message = await embed.send_embed(title=f"Searching for {prompt}...", color=discord.Color.yellow(), context=ctx)
+
+        video_id = await self._search_youtube(message=message, query=prompt)
+        message.edit(embed=discord.Embed(title="Downloading song..."))
+        song = Song(url=f"https://www.youtube.com/watch?v={video_id}")
+        queue.add(song=song)
+
+        await message.edit(embed=discord.Embed(
+            title=f"{song.title} has been added to the queue.",
+            color=discord.Color.blurple()
+        ))
+
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(QueueCog(bot=bot))
